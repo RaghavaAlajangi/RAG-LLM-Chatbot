c988419313ab0029ae616be82671480eeda3b63e
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-07-02 16:16:24 +0200

    fix: update cache folder path for HuggingFaceEmbeddings to use dynamic model directory

--- backend/langchain_utils/embedder.py
+++ backend/langchain_utils/embedder.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 from langchain.embeddings import HuggingFaceEmbeddings
+
+model_dir = Path(__file__).parents[2] / "llm_models" / "embedding"
+
 
 embedder = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5",
     model_kwargs={"device": "cuda"},
     encode_kwargs={"normalize_embeddings": True, "batch_size": 16},
-    cache_folder="llm_models/embeddings",
+    cache_folder=model_dir,
     multi_process=False,
 )
 
